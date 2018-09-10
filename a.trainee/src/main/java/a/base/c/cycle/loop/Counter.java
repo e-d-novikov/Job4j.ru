20958347067d76e75da4769589bd5d2ffacb88db
e-d-novikov <e.novikov@yahoo.com>
2018-09-10 17:51:21 +0500

    5.1. Подсчет суммы чётных чисел в диапазоне [#192]

---
+++ a.trainee/src/main/java/a/base/c/cycle/loop/Counter.java
@@ -0,0 +1,27 @@
+package a.base.c.cycle.loop;
+
+/**
+ * Class Counter.
+ *
+ * @author Egor Novikov
+ * E-mail: e.novikov@yahoo.com
+ * @version 1$
+ * @since 0.1
+ */
+public class Counter {
+  /**
+   * Method add calculates the sum of even numbers in the range.
+   * @param start - start of range;
+   * @param finish - end of range
+   * @return - sum of even numbers.
+   */
+  public int add(int start, int finish) {
+    int result = 0;
+    for (int  i = start; i <= finish; i++) {
+      if (i % 2 == 0) {
+        result = result + i;
+      }
+    }
+    return result;
+  }
+}
---
+++ a.trainee/src/main/java/a/base/c/cycle/loop/package-info.java
@@ -0,0 +1,8 @@
+package a.base.c.cycle.loop;
+/**
+ * Package loop.
+ * @author Egor Novikov
+ * E-mail: e.novikov@yahoo.com
+ * @version 1.0
+ * @since 0.1
+ */
