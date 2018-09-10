4d733b6d466c13159184d041c64cbe8d13269cb0
e-d-novikov <e.novikov@yahoo.com>
2018-09-10 13:44:24 +0500

    3.1. Элементарный калькулятор. [#185]

---
+++ a.trainee/src/main/java/a/base/a/types/package-info.java
@@ -0,0 +1,9 @@
+package a.base.a.types;
+
+/**
+ * In the package types of solution tasks from the Data Types section
+ * @author Egor Novikov
+ * E-mail: e.novikov@yahoo.com
+ * @version 1.0
+ * @since 0.1
+ */
