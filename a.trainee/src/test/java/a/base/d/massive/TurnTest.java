0571617acb3021ea3a468066b21b98c766bb8162
e-d-novikov <e.novikov@yahoo.com>
2018-09-10 20:42:46 +0500

    6.2. Перевернуть массив. [#4441]

---
+++ a.trainee/src/test/java/a/base/d/massive/TurnTest.java
@@ -0,0 +1,33 @@
+package a.base.d.massive;
+
+import org.junit.Test;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+/**
+ * Class TurnTest tests methods from class Turn.
+ *
+ * @author Egor Novikov
+ * E-mail: e.novikov@yahoo.com
+ * @version 1$
+ * @since 0.1
+ */
+public class TurnTest {
+
+  @Test
+  public void whenTurnArrayWithEvenAmountOfElementsThenTurnedArray() {
+    Turn turner = new Turn();
+    int[] input = new int[] {1, 2, 3, 4};
+    int[] result = turner.turn(input);
+    int[] expect = new int[] {4, 3, 2, 1};
+    assertThat(result, is(expect));
+  }
+
+  @Test
+  public void whenTurnArrayWithOddAmountOfElementsThenTurnedArray() {
+    Turn turner = new Turn();
+    int[] input = new int[] {1, 2, 3, 4, 5};
+    int[] result = turner.turn(input);
+    int[] expect = new int[] {5, 4, 3, 2, 1};
+    assertThat(result, is(expect));
+  }
+}
