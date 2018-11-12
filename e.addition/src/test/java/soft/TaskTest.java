4f8b4a357ba7594563c39c7876dd7c1d09aa6586
e-d-novikov <e.novikov@yahoo.com>
2018-11-13 01:50:11 +0500

    Task

--- e.addition/src/test/java/soft/TaskTest.java
+++ e.addition/src/test/java/soft/TaskTest.java
@@ -9,6 +9,7 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
 
 public class TaskTest {
+
     @Test
     public void whenSteps7864AndCycleLength1695() {
         int[] array = new int[]{0, 5, 10, 0, 11, 14, 13, 4, 11, 8, 8, 7, 1, 4, 12, 11};
