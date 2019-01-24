4184695b302742c48e388b886cfb3d0216c405da
e-d-novikov <e.d.novikov1990@yandex.ru>
2019-01-24 13:36:31 +0500

    change

--- 1_Trainee/src/test/java/b/oop/a/tracker/TrackerTest.java
+++ 1_Trainee/src/test/java/b/oop/a/tracker/TrackerTest.java
@@ -1,14 +1,12 @@
 package b.oop.a.tracker;
 
 import org.junit.Test;
-
 import java.io.*;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Properties;
-
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
@@ -17,9 +15,8 @@ public class TrackerTest {
 
     public Connection init() throws IOException, ClassNotFoundException, SQLException {
         Properties config = new Properties();
-        File file = new File(System.getProperty("user.dir") + "/src/main/java/b/oop/a/tracker/config/database.properties");
-        try (FileInputStream fin = new FileInputStream(file)) {
-            config.load(fin);
+        try (InputStream stream = getClass().getClassLoader().getResourceAsStream("database.properties")) {
+            config.load(stream);
         }
         Class.forName(config.getProperty("driver-class-name"));
         return DriverManager.getConnection(config.getProperty("url"), config.getProperty("username"), config.getProperty("password"));
